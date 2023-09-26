bd895cf726fd1a6fa4fbdc87503c260c03f71647
Yurika <c0a22039@edu.teu.ac.jp>
2023-09-26 16:41:27 +0900

    演習

--- ex01/pygame_image.py
+++ ex01/pygame_image.py
@@ -11,8 +11,28 @@ def main():
     bg_img2 = pg.transform.flip(bg_img,True,False)
     kk_img=pg.image.load("C:\\Users\\c0a22\\OneDrive\\ドキュメント\\CS2年後期\\ProjExD2023\\ex01\\fig\\3.png")
     kk_img=pg.transform.flip(kk_img,True,False)#上書き代入
-    kk_img2=pg.transform.rotozoom(kk_img,10,1.0)
-    kk_imgs=[kk_img,kk_img2]#回転した奴としてないやつのリスト
+
+
+    #kk_imgs=[]
+    #number= list(range(1, 10))
+    #for i in range[10]:
+
+    #kk_img2=pg.transform.rotozoom(kk_img,10,1.0)#回転画像
+    kk_img2=pg.transform.rotozoom(kk_img,5,1.0)#回転画像
+    kk_img3=pg.transform.rotozoom(kk_img,10,1.0)#回転画像
+        #kk_img.append(kk_imgs)
+        #i+=1
+        
+
+    #kk_img2=pg.transform.rotozoom(kk_img,2,1.0)#回転させる
+    #kk_img4=pg.transform.rotozoom(kk_img2,4,1.0)#回転させる
+    #kk_img6=pg.transform.rotozoom(kk_img4,6,1.0)#回転させる
+    #kk_img8=pg.transform.rotozoom(kk_img6,8,1.0)#回転させる
+    #kk_img10=pg.transform.rotozoom(kk_img8,10,1.0)#回転させる
+
+    #kk_imgs=[kk_img,kk_img2,kk_img4,kk_img6,kk_img8,kk_img10]#回転した奴としてないやつのリスト
+    kk_imgs=[kk_img,kk_img2,kk_img3]
+
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -23,7 +43,8 @@ def main():
 
         screen.blit(bg_img2, [-x, 0])#さいしょ
         screen.blit(bg_img, [1600-x, 0])#さいご(tmrが1600になったとき)
-        screen.blit(kk_imgs[tmr%2], [300, 200]) #こうかとんの読み込み
+        #screen.blit(kk_imgs[tmr%1], [300, 200]) #こうかとんの読み込み
+        screen.blit(kk_imgs[tmr%3], [300, 200]) 
     
         
         #screen.blit(kk_img, [300, 200]) #こうかとんの読み込み
