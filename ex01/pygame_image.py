1f26a43b0c716e5e49c01bbc7aac121de462d356
Yurika <c0a22039@edu.teu.ac.jp>
2023-09-26 16:11:15 +0900

    問6

--- ex01/pygame_image.py
+++ ex01/pygame_image.py
@@ -7,6 +7,8 @@ def main():
     screen = pg.display.set_mode((1600, 900)) #Surfaceを生成
     clock  = pg.time.Clock()
     bg_img = pg.image.load("C:\\Users\\c0a22\\OneDrive\\ドキュメント\\CS2年後期\\ProjExD2023\\ex01\\fig\\pg_bg.jpg") #画像の読み込み
+    bg_img2 = pg.image.load("C:\\Users\\c0a22\\OneDrive\\ドキュメント\\CS2年後期\\ProjExD2023\\ex01\\fig\\pg_bg.jpg")#反転画像
+    bg_img2 = pg.transform.flip(bg_img,True,False)
     kk_img=pg.image.load("C:\\Users\\c0a22\\OneDrive\\ドキュメント\\CS2年後期\\ProjExD2023\\ex01\\fig\\3.png")
     kk_img=pg.transform.flip(kk_img,True,False)#上書き代入
     kk_img2=pg.transform.rotozoom(kk_img,10,1.0)
@@ -16,11 +18,13 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
-        if tmr%2==0:
-            screen.blit(kk_imgs[0], [300, 200]) #こうかとんの読み込み
-        else:
-            screen.blit(kk_imgs[1], [300, 200]) #こうかとんの読み込み
+        x=tmr%1600
+            
+
+        screen.blit(bg_img2, [-x, 0])#さいしょ
+        screen.blit(bg_img, [1600-x, 0])#さいご(tmrが1600になったとき)
+        screen.blit(kk_imgs[tmr%2], [300, 200]) #こうかとんの読み込み
+    
         
         #screen.blit(kk_img, [300, 200]) #こうかとんの読み込み
         pg.display.update()
